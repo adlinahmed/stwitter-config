0db9359b188d4aa56a7b0d8f0ae4ba679e9ff0f0
adlinahmed <59899886+adlinahmed@users.noreply.github.com>
2021-05-08 16:35:58 -0500

    Delete stwitter-service directory

--- stwitter-service/src/main/java/com/trilogyed/stwitter/exception/NoSuchPostException.java
+++
@@ -1,7 +0,0 @@
-package com.trilogyed.stwitter.exception;
-
-public class NoSuchPostException extends RuntimeException{
-    public NoSuchPostException(String message) {
-        super(message);
-    }
-}
