c3948a53aad9d43aab87f2578f17a329eb77f844
adlinahmed <59899886+adlinahmed@users.noreply.github.com>
2021-05-08 16:35:16 -0500

    Delete comment-service directory

--- comment-service/src/main/java/com/trilogyed/comment/InvalidIdException.java
+++
@@ -1,7 +0,0 @@
-package com.trilogyed.comment;
-
-public class InvalidIdException  extends RuntimeException {
-    public InvalidIdException(int id) {
-        super("Invalid id: " + id);
-    }
-}
--- comment-service/src/main/java/com/trilogyed/comment/dao/CommentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.trilogyed.comment.dao;
-
-import com.trilogyed.comment.dto.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepo extends JpaRepository<Comment, Integer> {
-}
